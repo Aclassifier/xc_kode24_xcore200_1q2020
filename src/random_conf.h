0678248a2d6015150c7d1d1ddf1c0df39d70294e
Aclassifier <oyvind.teig@teigfam.net>
2020-02-13 10:14:14 +0100

    Ver 0.50 2020.02.13 Starts workers in 4 sequences and workers simulate random work

---
+++ src/random_conf.h
@@ -0,0 +1,17 @@
+/*
+ * random_conf.h
+ *
+ *  Created on: 12. feb. 2020
+ *      Author: teig
+ */
+
+
+#ifndef RANDOM_CONF_H_
+#define RANDOM_CONF_H_
+
+#define RANDOM_ENABLE_HW_SEED 1
+#define __XS1_L__
+
+#else
+    #error Nested include RANDOM_CONF_H_
+#endif
